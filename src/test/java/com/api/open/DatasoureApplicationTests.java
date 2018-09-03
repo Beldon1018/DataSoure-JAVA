98769ea9ea7aee7e170958c560766ab4b41e8cd3
zhangzhichao <zhangzhichaolove@vip.qq.com>
2018-09-03 10:06:12 +0800

    项目创建。

---
+++ src/test/java/com/api/open/DatasoureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.api.open;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasoureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
